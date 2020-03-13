bf3686071a0cd5e5c326af957df47437d3286fb5
Sulejman Bijedic <sule.bijedic@gmail.com>
2020-03-13 15:16:00 +0100

    Add bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,26 @@
+# Bubble sort algorithm
+
+
+def bubbleSort(dataset):
+
+    print("\nInitial state: ", dataset, '\n')
+
+    for i in range(len(dataset) - 1, 0, -1):
+        for j in range(i):
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp
+
+        print("Current state: ", dataset)
+
+
+def main():
+
+    list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+    bubbleSort(list1)
+    print("\nResult: ", list1, '\n')
+
+
+if __name__ == "__main__":
+    main()
