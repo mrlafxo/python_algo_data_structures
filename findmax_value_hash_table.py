f79c691cff60e7afa34287dc5feb5059063f4b05
Sulejman Bijedic <sule.bijedic@gmail.com>
2020-03-16 17:24:36 +0100

    Add findmax value w/ hash table

---
+++ findmax_value_hash_table.py
@@ -0,0 +1,25 @@
+# use a recursive algorithm to find a maximum value
+
+# declare a list of values to operate on
+items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+
+def find_max(items):
+    # breaking condition
+    if len(items) == 1:
+        return items[0]
+
+    # otherwise get the first item and call the function
+    # again to operate on the rest of the list
+    op1 = items[0]
+    print(op1)
+    op2 = find_max(items[1:])
+    print(op2)
+
+
+    if op1 > op2:
+        return op1
+    else:
+        return op2
+
+# test the function
+print("The max value is: ", find_max(items))
